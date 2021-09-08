5f5727bcee63497ba3bb3aa4f72e988a0aeda48e
cihanyantir <cihan.yantir@ogr.sakarya.edu.tr>
2021-09-09 01:48:09 +0300

    Her entity class implemente edileceği için interface method yazıldı.

---
+++ CORE API/IService/IGenericService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CORE_API.IService
+{
+    public interface IGenericService<T>
+    {
+        List<T> GetAll();
+        T GetById(int id);
+        List<T> Insert(T item);
+        List<T> Delete(int id);
+
+        
+        
+
+    }
+}
