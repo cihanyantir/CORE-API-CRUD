d9ad3c5f6874ca48bda5c7c363b8dd8cdf5cafb1
cihanyantir <cihan.yantir@ogr.sakarya.edu.tr>
2021-09-09 19:19:51 +0300

    Basic CRUD proccessleri yazıldı

--- CORE API/Service/OrderService.cs
+++ CORE API/Service/OrderService.cs
@@ -1,11 +1,46 @@
-﻿using System;
+﻿using CORE_API.IService;
+using CORE_API.Repository.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CORE_API.Service
 {
-    public class OrderService
+    public class OrderService : IGenericService<Order>
     {
+        List<Order> order = new List<Order>();
+
+        public OrderService()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                order.Add(new Order() { Id = i, OrderDate="2021", Quantity=25+i });
+            }
+        }
+        public List<Order> Delete(int id)
+        {
+            order.RemoveAll(x => x.Id == id);
+            return order;
+
+        }
+
+        public List<Order> GetAll()
+        {
+            return order;
+
+        }
+
+        public Order GetById(int id)
+        {
+            return order.Where(x => x.Id == id).SingleOrDefault();
+        }
+
+        public List<Order> Insert(Order item)
+        {
+            order.Add(item);
+            return order;
+        }
     }
 }
+
--- CORE API/Service/ProductService.cs
+++ CORE API/Service/ProductService.cs
@@ -1,11 +1,45 @@
-﻿using System;
+﻿using CORE_API.IService;
+using CORE_API.Repository.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CORE_API.Service
 {
-    public class ProductService
+    public class ProductService : IGenericService<Product>
     {
+        List<Product> _product = new List<Product>();
+
+        public ProductService()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                _product.Add(new Product() { Id = i, Name = "Urun" + i, Stock=60+i});
+            }
+        }
+        public List<Product> Delete(int id)
+        {
+            _product.RemoveAll(x => x.Id == id);
+            return _product;
+
+        }
+
+        public List<Product> GetAll()
+        {
+            return _product;
+
+        }
+
+        public Product GetById(int id)
+        {
+            return _product.Where(x => x.Id == id).SingleOrDefault();
+        }
+
+        public List<Product> Insert(Product item)
+        {
+            _product.Add(item);
+            return _product;
+        }
     }
 }
